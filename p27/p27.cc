f0666714406b1ab3d85886924c08e47f8dcd0d77
ZureTz <2819058133@qq.com>
2024-12-19 13:23:45 +0800

    Tested copilot

--- p27/p27.cc
+++ p27/p27.cc
@@ -4,27 +4,39 @@
 class Solution {
 public:
   int removeElement(std::vector<int> &nums, int val) {
-    // Do nothing if size is 0
-    if (nums.size() == 0) {
+    // Return 0 if the array is empty
+    if (nums.empty()) {
       return 0;
     }
 
     int remainingNumsCount = nums.size();
-    std::vector<int>::iterator itBegin = nums.begin(), itEnd = nums.end() - 1;
+    auto itBegin = nums.begin();
+    auto itEnd = nums.end() - 1;
+
+    // Move itEnd from back to front until finding the first element not equal
+    // to val
     while (itBegin <= itEnd && *itEnd == val) {
       itEnd--;
       remainingNumsCount--;
     }
 
+    // Return if all elements are equal to val
     if (itEnd < itBegin) {
       return remainingNumsCount;
     }
 
+    // Traverse the array from front to back, swapping elements equal to val to
+    // the end of the array
     while (itBegin <= itEnd) {
-      while (itBegin <= itEnd && *itBegin == val) {
+      if (*itBegin == val) {
         std::swap(*itBegin, *itEnd);
         itEnd--;
         remainingNumsCount--;
+        // Ensure the element pointed to by itEnd is not equal to val
+        while (itBegin <= itEnd && *itEnd == val) {
+          itEnd--;
+          remainingNumsCount--;
+        }
       }
       itBegin++;
     }
